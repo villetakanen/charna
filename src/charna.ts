f59247e1a0f6729be8546e7f4f08f1d2b4eb4896
Ville Takanen <vitkukissa@gmail.com>
2021-11-28 22:26:04 +0200

    fix: moved CharacterSheet to a separate file

--- src/charna.ts
+++ src/charna.ts
@@ -1,5 +1,7 @@
 
-export interface Stat {
+import { CharacterSheet } from './CharacterSheet'
+
+interface Stat {
   id: string
   value: number|boolean|string
   min?: number
@@ -7,33 +9,15 @@ export interface Stat {
   derived?: boolean
 }
 
-export interface CharacterSheetModel {
+interface CharacterSheetModel {
   name: string
   system: string
   properties: { [key: string]: string }
   stats: { [key: string]: Stat }
 }
 
-export class CharacterSheet {
-  id: string
-  private _model: CharacterSheetModel|undefined
-
-  constructor(id: string) {
-    this.id = id
-  }
-
-  get (statName: string): Stat {
-    return this._model ? {...this._model.stats[statName]} : {id: statName, value: 0}
-  }
-  set (statName: string, value: number|boolean|string) {
-    if (this._model) this._model.stats[statName].value = value
-  }
-
-
-  set model(model: CharacterSheetModel) {
-    this._model = model
-  }
-  get model(): CharacterSheetModel {
-    return this._model || { name: '', system: '', properties: {}, stats: {} }
-  }
+export {
+  Stat,
+  CharacterSheetModel,
+  CharacterSheet
 }
